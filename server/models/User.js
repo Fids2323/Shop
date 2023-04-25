6b1a12ea06353122d03ddc0e41dcd54c3e26dd7a
Dmitry Homskiy <workfids23@gmail.com>
2023-04-25 22:03:42 +0300

    add auth.routes

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,6 @@ const {Schema, model} = require("mongoose");
 
 const schema = new Schema(
 	{
-		name: {type: String, required: true},
 		email: {type: String, required: true, unique: true},
 		password: {type: String},
 		image: String,
